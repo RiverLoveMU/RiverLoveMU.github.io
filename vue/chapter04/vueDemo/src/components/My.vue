c44ee5c372c7fdafc7b9369118c8b65cd16fb99e
HeLiu <723694425@qq.com>
2017-08-10 18:27:04 +0800

    0810

---
+++ vue/chapter04/vueDemo/src/components/My.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="my">
+    <h1>这是我自己的组件</h1>
+    <h2>{{msg}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'my',
+  data () {
+    return {
+      msg: '哈哈哈哈哈哈哈啊哈哈哈哈哈哈'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1{
+    background: #ccc;
+    color:red;
+  }
+</style>
